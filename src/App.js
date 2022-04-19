6e5f7af662b7af9df428a137b32e65eef99324ee
san man <sance1921@gmail.com>
2022-04-19 16:14:24 +0200

    update toggleReminder in server

--- src/App.js
+++ src/App.js
@@ -8,7 +8,33 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
-  // function pour supprimer une tachee(task)
+  // GET TASKS (function pour fetcher les taches au server)
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    console.log('data', data);
+    return data;
+  };
+
+  // GET TASK (function pour fetcher une tache au server)
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    console.log('data', data);
+    return data;
+  };
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  // DELETE TASK (function pour supprimer une tache)
+
   const deleteTask = async (id) => {
     // setTasks(tasks.filter((task) => task.id !== id));
     await fetch(`http://localhost:5000/tasks/${id}`, {
@@ -18,16 +44,8 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  // function pour faire le rappel de la tache avec le double click
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      )
-    );
-  };
+  // ADD TASK (function pour ajouter une tache)
 
-  // function pour ajouter une tache(task)
   const addTask = async (task) => {
     // const id = Math.floor(Math.random() * 10000) + 1;
     // const newTask = { id, ...task };
@@ -49,21 +67,29 @@ function App() {
     setShowAddTask(!showAddTask);
   };
 
-  //function pour fetcher les tasks au server
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks');
-    const data = await res.json();
-    console.log('data', data);
-    return data;
-  };
+  // function pour faire le rappel de la tache avec le double click
+  const toggleReminder = async (id) => {
+    const taskToToogle = await fetchTask(id);
+    console.log('taskToToogle', taskToToogle);
+    const upTask = { ...taskToToogle, reminder: !taskToToogle.reminder };
+    console.log('upTask', upTask);
 
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks();
-      setTasks(tasksFromServer);
-    };
-    getTasks();
-  }, []);
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(upTask)
+    });
+    const data = res.json();
+    setTasks(data);
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
